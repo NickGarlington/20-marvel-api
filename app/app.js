571fb6fff5ca6aa15159735d90919ca7d7bd174a
Nick Garlington <nickgarlington@Nicks-MacBook-Pro-2.local>
2016-04-26 14:02:30 -0500

    just styling

--- app/app.js
+++ app/app.js
@@ -1,2 +1,30 @@
 import 'es6-promise';
 import 'whatwg-fetch';
+
+
+import Character from "./modules/Character";
+import Event from "./modules/Event";
+
+function character(ctx) {
+  let placeholderLeft = document.querySelector("#placeholderLeft");
+  placeholderLeft.innerHTML = "";
+  let placeholderRight = document.querySelector("#placeholderRight");
+  placeholderRight.innerHTML = "";
+  let character = new Character(ctx.params.name);
+}
+
+function event(ctx){
+  let placeholderLeft = document.querySelector("#placeholderLeft");
+  placeholderLeft.innerHTML = "";
+  let placeholderRight = document.querySelector("#placeholderRight");
+  placeholderRight.innerHTML = "";
+  let event = new Event(ctx.params.id);
+}
+
+// character();
+
+
+page("/", character);
+page("/characters/:name", character);
+page("/events/:id", event);
+page();
